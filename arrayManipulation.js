8331d2decad0477ed3de601edc1795bc2d1ddadb
Aforve-Peter <pkaforve@st.ug.edu.gh>
2024-05-20 09:42:12 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -13,3 +13,15 @@ console.log(processArray [1,2,4,5]);
 const num =  [1,2,3,4,5,6,7]
 const results = processArray(num)
 console.log(results);
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((str, index) => {
+        if (processedNumbers[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+const stringsArray = ["Hello", "World", "This", "Is", "Test"];
+const formattedStrings = formatArrayStrings(stringsArray, results)
+console.log(formattedStrings)
